b3d3b8cc6c3871877ea2938fe918194d783b22fa
High-Hill <k196336@kansai-u.ac.jp>
2018-04-01 23:04:06 +0900

    add itp1_1_c and otoshidama

---
+++ itp1/itp1_1_c.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // a = length, b = breadth
+    int a, b;
+
+    cin >> a >> b;
+    cout << a * b << " " << (a + b) * 2 << endl;
+
+    return 0;
+}
