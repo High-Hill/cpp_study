5a7d2b0e22abf162b3750ff6883acf109ff8903c
High-Hill <k196336@kansai-u.ac.jp>
2018-04-13 22:35:08 +0900

    add itp1_4_b

---
+++ itp1/itp1_4_b.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int main(){
+    double r;
+    double p = 3.141592653589;
+    cin >> r;
+    printf("%lf %lf\n", r * r * p, 2 * r * p);
+    return 0;
+}
